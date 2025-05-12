7c953d87f01d41d4afafa26c09ae61d5190b135c
Rohit Kumar Sahu <rrohitkumarsahu2002@gmail.com>
2025-05-12 14:32:40 +0530

    bug fix

--- routes.js
+++ routes.js
@@ -23,7 +23,7 @@ const sendNotification = async (fcmtoken, title, body) => {
         if (error.code === 'messaging/registration-token-not-registered') {
             console.log(`Token ${fcmtoken} is invalid, removing from database.`);
             // Remove the token from the User document
-            await Schema.updateOne(
+            await User.updateOne(
                 { fcmToken: fcmtoken },
                 { $unset: { fcmToken: "" } }
             );
